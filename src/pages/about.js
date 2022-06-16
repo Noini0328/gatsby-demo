3481537affa303dfdd8121f35600d2b39ba4c8e7
noini0328 <noini.0328@gmail.com>
2022-06-16 20:09:40 +0900

    3 progress3.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,14 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { Helmet } from 'react-helmet';
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
+      <Helmet>
+        <meta name="description" content="このページはAboutページです。" />
+        <meta name="robots" content="all" />
+      </Helmet>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
   )

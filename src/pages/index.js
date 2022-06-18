cf837f7951d9f296cda334b58aae80a338305b0b
noini0328 <noini.0328@gmail.com>
2022-06-18 16:59:10 +0900

    header, footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,25 @@ import Layout from '../components/layout'
 import { Helmet } from 'react-helmet';
 import { StaticImage } from 'gatsby-plugin-image'
 
+// parts
+import Header from "../components/header.js"
+import Footer from "../components/footer.js"
+
 const IndexPage = () => {
   return (
-
+    
     <Layout pageTitle="Home Page5-2">
       <Helmet>
         <meta name="description" content="このページはIndexページです。" />
         <meta name="robots" content="all" />
       </Helmet>
+      <Header/>
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair test"
         src="../images/clifford2.jpg"
       />
+      <Footer/>
     </Layout>
   )
 }

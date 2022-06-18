09a647f4bd97ed86734080786840cc52684442cc
noini0328 <noini.0328@gmail.com>
2022-06-18 16:09:21 +0900

    mdx test4

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,5 @@ module.exports = {
     },
     __key: "blog"
   },
-  "gatsby-plugin-mdx",
 ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => {
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair test"
-        src="../images/clifford.jpg"
+        src="../images/clifford2.jpg"
       />
     </Layout>
   )
